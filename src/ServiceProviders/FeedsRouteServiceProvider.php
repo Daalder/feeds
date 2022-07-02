07d4b28144cc798afe24fdff9c40e7c14a4dc9de
Anis Alkomem <anis@pionect.nl>
2022-07-02 10:41:00 +0200

    [FIX] tests

--- src/ServiceProviders/FeedsRouteServiceProvider.php
+++ src/ServiceProviders/FeedsRouteServiceProvider.php
@@ -5,21 +5,25 @@ namespace Daalder\Feeds\ServiceProviders;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use Pionect\Daalder\BackofficeServiceProvider;
 
 class FeedsRouteServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $middleware = hook('authenticated-middleware',
-            ['web', 'auth', 'global_view_shares', 'language_switch', 'set_store']);
+        if (class_exists(BackofficeServiceProvider::class)) {
+            $middleware = hook('authenticated-middleware',
+                ['web', 'auth', 'global_view_shares', 'language_switch', 'set_store']);
 
-        $group_attributes = [
-            'domain' => Str::after(config('app.url'), '://'),
-            'middleware' => $middleware,
-        ];
-        Route::group($group_attributes, function () {
-            require __DIR__ . '/../../routes/feeds.php';
-        });
+            $group_attributes = [
+                'domain' => Str::after(config('app.url'), '://'),
+                'middleware' => $middleware,
+            ];
+            Route::group($group_attributes, function () {
+                require __DIR__ . '/../../routes/feeds.php';
+            });
+
+        }
     }
 
     public function register()
